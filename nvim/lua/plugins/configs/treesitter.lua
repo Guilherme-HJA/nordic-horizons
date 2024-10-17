60daf1edf907d47fa9dc55fd99f3bea224d352e9
Guilherme-HJA <Guilherme.hjaraujo@proton.me>
2024-10-17 16:56:14 -0300

    Initial commit

---
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,28 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "html",
+    "css",
+    "scss",
+    "vue",
+    "javascript",
+    "typescript",
+    "tsx",
+    "toml",
+    "json",
+    "python",
+    "haskell",
+    "elixir",
+    "heex",
+    "eex",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
